df14e158b2dab1bfd55162e50fdb8ab1e3e3ede7
lread <lee@dlread.com>
2020-12-29 14:24:37 -0500

    Get rid of some duplication in deps.edn
    Move test gen support to script/gen_local_tests.clj

---
+++ script/gen_local_tests.clj
@@ -0,0 +1,41 @@
+#!/usr/bin/env bb
+
+(ns gen-local-tests
+  (:require [babashka.classpath :as cp]))
+
+(cp/add-classpath "./script")
+(require '[helper.env :as env]
+         '[helper.shell :as shell]
+         '[helper.status :as status])
+
+(defn- gen-tests
+  ([] (gen-tests nil))
+  ([target-root]
+   (let [docs ["README.adoc"
+               "doc/example.adoc"
+               "doc/example.md"]]
+
+     (shell/command ["clojure" "-X:test-doc-blocks" "gen-tests"
+
+                     ":target-root" (pr-str target-root)
+                     ":docs" (str docs)]))))
+
+(defn main [args]
+  (env/assert-min-versions)
+  (let [[cmd] args]
+    (cond
+      (= "regen-expected" cmd)
+      (do
+        (status/line :info "Regnerating expected tests for local documents.")
+        (gen-tests "test-resources/expected"))
+
+      (nil? cmd)
+      (do
+        (status/line :info "Generating tests for local documents.")
+        (gen-tests))
+
+      :else
+      (status/fatal (str "Invalid cmd: " cmd " - see docs for usage.")))))
+
+(main *command-line-args*)
+nil
