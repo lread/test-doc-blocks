bee7e65b95c907c75b58b5cb3088d8c69f4ed6e8
lread <lee@dlread.com>
2021-02-05 17:14:53 -0500

    Refine release workflow
    Automatically add a gritty details compare link back to github
    for detailed change review.
    
    Add naive validation for change log.
    - fail if unreleased section is missing or empty
    - fail if unreleased breaking changes is present and empty

--- script/release.clj
+++ script/release.clj
@@ -20,6 +20,11 @@
   (doseq [dir ["target" ".cpcache"]]
     (fs/delete-file-recursively dir true)))
 
+(defn- last-release-tag []
+  (->  (shell/command ["git" "describe" "--abbrev=0" "--match" "v[0-9]*"] {:out :string})
+       :out
+       string/trim))
+
 (defn- repo-commit-count
   "Number of commits in the repo"
   []
@@ -47,7 +52,7 @@
 
 (defn- update-file! [fname match replacement]
   (let [old-content (slurp fname)
-        new-content (string/replace old-content match replacement)]
+        new-content (string/replace-first old-content match replacement)]
     (if (= old-content new-content)
       (status/fatal (str  "Expected " fname " to be updated."))
       (spit fname new-content))))
@@ -58,11 +63,59 @@
                 #"( +\{:extra-deps \{lread/test-doc-blocks \{:mvn/version \").*(\"\}\})"
                 (str  "$1" version "$2")))
 
-(defn- update-changelog! [version]
+(defn- adoc-section-search[content find-section]
+  (cond
+    (re-find (re-pattern  (str  "(?ims)^=+ " find-section " *$\\s+(^=|\\z)")) content)
+    :found-with-no-text
+
+    (re-find (re-pattern  (str  "(?ims)^=+ " find-section " *$")) content)
+    :found
+    
+    :else
+    :not-found))
+
+(defn- validate-changelog 
+  "Certainly not fool proof, but should help for common mistakes"
+  []
+  (status/line :info "Validating change log")
+  (let [content (slurp "CHANGELOG.adoc")
+        unreleased-status (adoc-section-search content "Unreleased")
+        unreleased-breaking-status (adoc-section-search content "Unreleased Breaking Changes")]
+    (case unreleased-status
+      :found (status/line :detail "✅ Unreleased section found with descriptive text.")
+      :found-with-no-text (status/line :detail "❌ Unreleased section seems empty, please put some descriptive text in there to help our users understand what is in the release.")
+      :not-found (status/line :detail "❌ Unreleased section not found, please add one."))
+    (case unreleased-breaking-status
+      :found (status/line :detail "✅ Unreleased Breaking Changes section found with descriptive text")
+      :found-with-no-text (status/line :detail (str "❌ Unreleased breaking changes section found but seems empty.\n"
+                                                    "   Please put some descriptive text in there to help our users understand what is in the release\n"
+                                                    "   OR delete the section if there are no breaking changes for this release."))
+      :not-found (status/line :detail "✅ Unreleased Breaking Changes section not found, assuming no breaking changes for this release."))
+      
+    (if (or (not (= :found unreleased-status))
+            (= :found-with-no-text unreleased-breaking-status))
+      (status/fatal "Changelog needs some love")
+      (status/line :detail "Changelog looks good for update by release workflow."))
+    {:unreleased unreleased-status
+     :unreleased-breaking unreleased-breaking-status}))
+
+(defn- update-changelog! [version last-version {:keys [unreleased-breaking]}]
   (status/line :info (str "Updating Change Log unreleased headers to release " version))
   (update-file! "CHANGELOG.adoc"
-               #"(?im)^(=+) +unreleased *$"
-               (str "$1 Unreleased\n\n$1 v" version)))
+                #"(?ims)^(=+) +unreleased *$(.*?)(^=+)"
+                (str "$1 Unreleased\n\n$1 v" version "$2"
+                     (when last-version
+                       (str
+                        "https://github.com/lread/test-doc-blocks/compare/"
+                        last-version
+                        "\\\\..."  ;; single backslash is escape for AsciiDoc
+                        version
+                        "[Gritty details of changes for this release]\n\n"))
+                     "$3"))
+  (when (= :found unreleased-breaking)
+    (update-file! "CHANGELOG.adoc"
+                  #"(?ims)(=+) +unreleased breaking changes *$"
+                  (str "$1 v" version))))
 
 (defn- create-jar! [version]
   (status/line :info (str "Updating pom.xml and creating jar for version " version))
@@ -111,10 +164,15 @@
       (status/line :warn (str  "Informing cljdoc did not seem to work, exited with " exit-code)))))
 
 (defn- main []
+  (status/line :info "Attempting release")
   (clean)
-  (let [target-version (calculate-version)]
+  (let [changelog-status (validate-changelog)
+        target-version (calculate-version)
+        last-version (last-release-tag)]
+    (status/line :detail (str "Last version released: " (or last-version "<none>")))
+    (status/line :detail (str "Target version:        " target-version))
     (update-readme! target-version)
-    (update-changelog! target-version)
+    (update-changelog! target-version last-version changelog-status)
     (create-jar! target-version)
     (deploy-jar!)
     (commit-changes! target-version)
